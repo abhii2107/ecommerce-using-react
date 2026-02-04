99f557e14dfd93414f912839b9c470c923e089c3
abhii2107 <bhatiaabhishek776@gmail.com>
2026-02-04 06:41:27 +0530

    api fetching

--- react_website/src/context/ProductContext.jsx
+++ react_website/src/context/ProductContext.jsx
@@ -1,19 +1,57 @@
-
-import { createContext, useContext } from "react";
+import axios from "axios"
+import { createContext, useContext, useEffect, useReducer } from "react";
 
 //create context
- 
-export const AppContext = createContext(); 
+
+export const AppContext = createContext();
+const Api = "https://fakestoreapi.com/products"
+const intialState = {
+    isLoading: false,
+    isError: false,
+    products: [],
+    featureProducts: [],
+}
+
 
 //context provider
 
-export const AppProvider = ({children}) => {
-    return <AppContext.Provider value={{myName: "Abhi"}}>
+export const AppProvider = ({ children }) => {
+
+    const [state, dispatch] = useReducer(reducer, intialState);
+
+    const getProducts = async (url) => {
+        dispatch({type:"SET_LOADING"});
+        try {
+            const res = await axios.get(url);
+
+            const products = await res.data.products;
+
+            products = products.map((item,i) => {
+                return{
+                    ...item,
+                    featuerd:i<6
+                }
+            })
+
+            dispatch({ type: "SET_API_DATA", payload: products })
+        } catch (error) {
+            dispatch({type:"API_ERROR"});
+        }
+
+    }
+
+    useEffect(() => {
+        getProducts(Api);
+    }, [])
+
+
+    return (<AppContext.Provider value={{ ...state }}>
         {children}
     </AppContext.Provider>
-} 
+    )
+}
 
 //custom hooks
 export const useProductContext = () => {
     return useContext(AppContext);
-};
+}; 
