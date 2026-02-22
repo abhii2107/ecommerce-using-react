5166030f9a13800baba650659f1f5866b62e0701
Abhishek <bhatiaabhishek776@gmail.com>
2026-02-22 16:46:33 +0530

    css changes

--- react_website/src/pages/Cart.jsx
+++ react_website/src/pages/Cart.jsx
@@ -1,9 +1,10 @@
 import { CartItem } from "../components/CartItem";
 import { useCartContext } from "../context/CartContext"
 import { NavLink } from "react-router-dom";
+import { FormatPrice } from "../Helpers/FormatPrice";
 
 export const Cart = () => {
-    const {cart, clearCart} = useCartContext();
+    const {cart, clearCart,total_price,shipping_fee} = useCartContext();
     
     if(cart.length === 0){
         return(
@@ -39,6 +40,29 @@ export const Cart = () => {
                         <button className="continue-btn">Continue Shopping</button>
                     </NavLink>
                     <button className="clear-btn" onClick={clearCart}>Clear cart</button>
+                </div>
+
+                {/* order total amount */}
+                <div className="order-total--amount">
+                    <div className="order-total--subdata">
+                        <div className="subtotal">
+                            <p>Subtotal:</p>
+                            <p><FormatPrice price={total_price}/></p>
+                        </div>
+                        <div className="shipping_fee">
+                            <p>Shipping Fee:</p>
+                            <p>
+                                <FormatPrice price={shipping_fee}/>
+                            </p>
+                        </div>
+                        <hr />
+                        <div className="order_total">
+                            <p>Order Total:</p>
+                            <p>
+                                <FormatPrice price={shipping_fee+total_price}/>
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
